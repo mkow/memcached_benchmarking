54587c79d0925d3f00e75d22bd713c351ed12f1e
Michał Kowalczyk <mkow@invisiblethingslab.com>
2023-03-22 02:52:29 +0100

    fixup! tmp
    Signed-off-by: Michał Kowalczyk <mkow@invisiblethingslab.com>

--- runner.py
+++ runner.py
@@ -236,7 +236,7 @@ def print_matrix(rows, cols, m):
         print()
 
 def render_heatmap(rows, cols, m, output_path):
-    x_trim = 10
+    x_trim = 20
     y_trim = 19
     harvest = np.array([
         [0.8]*y_trim for _ in range(x_trim)
